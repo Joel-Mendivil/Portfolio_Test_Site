c59a6b5202ed415980d43d766c8f7498dde94313
Joel <jmendivil171@west-mec.org>
2020-08-18 15:31:46 -0700

    Added some of the code for the website in JavaScript

---
+++ skills_color.js
@@ -0,0 +1,13 @@
+"use strict";
+
+function changeBackgroundColor(){
+    body.style.backgroundColor = "rgb(0,0,139)";
+}
+
+function changeTitleColor(){
+    title.style.backgroundColor = "Yellow"
+}
+
+function highlightColor(){
+    em.style.Color = "Yellow";
+}
